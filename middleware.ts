c633b0c8eab5ce0e20ae45805803c351f9b7a393
Hanif2905 <hanifalmahmud2905@gmail.com>
2026-09-11 18:20:33 +0700

    remove admin middleware authentication

--- middleware.ts
+++ middleware.ts
@@ -4,51 +4,9 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
 
-
-  const token = request.cookies.get(
-    "next-auth.session-token"
-  );
-
-
-  const pathname = request.nextUrl.pathname;
-
-
-
-  // halaman admin
-  const isAdminRoute =
-    pathname.startsWith("/admin");
-
-
-  // halaman login
-  const isLoginPage =
-    pathname.startsWith("/admin/login");
-
-
-
-  // kalau masuk admin tapi belum login
-  // kecuali halaman login
-
-  if(
-    isAdminRoute &&
-    !isLoginPage &&
-    !token
-  ){
-
-    return NextResponse.redirect(
-      new URL(
-        "/admin/login",
-        request.url
-      )
-    );
-
-  }
-
-
-
   return NextResponse.next();
 
 }
-
 
 
 export const config = {
